3a7e2dbbe22af1beeb3e20b223401da2d4482c4d
Bertrand Caron <bertrand.caron@cascade.app>
2024-11-19 16:12:08 +1000

    Feat: Ignore errors in post step

--- src/post.ts
+++ src/post.ts
@@ -1,16 +1,13 @@
 import * as fs from "fs";
 import * as path from "path";
 
-import * as core from "@actions/core";
-
 import * as common from "./common";
 
 try {
     post();
 } catch (err) {
-    if (err instanceof Error) {
-        core.setFailed(err);
-    }
+    console.warn('Ignoring error during post step:');
+    console.warn(err);
 }
 
 /**
